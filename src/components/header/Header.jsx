8ae92127a8fd7bdf56e0992ca6b500d3aa5b1d2a
mfcastilho <fredericocastilho@hotmail.com>
2023-04-11 04:08:02 -0300

    header structure finalized

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+
+
+function Header(){
+     return(
+        <header className="header">
+
+            <h3 className="header-title">Where in the world?</h3>
+            
+            <div className="header-right-part">
+
+               <div className="dark-mode-box">
+                    {/* <img  src="../public/moon.svg" alt="" className="dark-mode-img" /> */}
+                    <h5 className="dark-mode title"></h5>
+               </div>
+
+               <div className="light-mode-box active">
+                    {/* <img  src="../public/sunny-outline.svg" alt="" className="light-mode-img" /> */}
+                    <h5 className="light-mode title"></h5>
+               </div>
+
+            </div>
+
+        </header>
+    )
+}
+
+export default Header;
