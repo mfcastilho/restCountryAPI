3b15aa07a437cf6459786f55c112659bf7a6a6b7
mfcastilho <fredericocastilho@hotmail.com>
2023-04-12 04:18:36 -0300

    header structure and home top part finalized

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,13 +11,13 @@ function Header(){
             <div className="header-right-part">
 
                <div className="dark-mode-box">
-                    {/* <img  src="../public/moon.svg" alt="" className="dark-mode-img" /> */}
-                    <h5 className="dark-mode title"></h5>
+                    <img  src="../public/moon-outline.svg" alt="" className="dark-mode-img" />
+                    <h5 className="dark-mode title">Dark Mode</h5>
                </div>
 
                <div className="light-mode-box active">
-                    {/* <img  src="../public/sunny-outline.svg" alt="" className="light-mode-img" /> */}
-                    <h5 className="light-mode title"></h5>
+                    <img  src="../public/sunny-outline.svg" alt="" className="light-mode-img" />
+                    <h5 className="light-mode title">Light Mode</h5>
                </div>
 
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,20 @@ function Home(){
      return(
         <div className="home-container">
            
+           <div className="home-top-part-box">
+            <form action="" className="search-country-form">
+                <input type="text" className="search-country-input" />
+            </form>
+
+            <select name="region" id="" className="region-select-list">
+                <option value="">Filter by Region</option>
+                <option value="Africa">África</option>
+                <option value="America">América</option>
+                <option value="Asia">Ásia</option>
+                <option value="Europa">Europa</option>
+                <option value="Oceania">Oceania</option>
+            </select>
+           </div>
         </div>
     )
 }
