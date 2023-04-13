e7a31965147321eb0ccc0771c6d0bfe5634bfe2e
mfcastilho <fredericocastilho@hotmail.com>
2023-04-13 03:50:45 -0300

    home top part structure and style finalized

--- src/components/home-top-part/HomeTopPart.jsx
+++ src/components/home-top-part/HomeTopPart.jsx
@@ -1,23 +1,42 @@
 import React from "react";
+import "./HomeTopPart.css"
 
 
 function HomeTopPart(){
 
 
+    function InsertinMarginBottomIntheTagsOptions(){
+        const options = document.querySelectorAll("option");
+
+        for(let i = 0; i < options.length; i++){
+            console.log(options[i].value);
+            options[i].style.marginBottom = "10px";
+        }
+    }
+
+    InsertinMarginBottomIntheTagsOptions()
+
      return(
           
           <div className="home-top-part-box">
                 <form action="" className="search-country-form">
+                    <button className="input-button"><img src="../public/lupa.svg" alt="" /></button>
                     <input type="text" className="search-country-input" />
                 </form>
 
-                <select name="region" id="" className="region-select-list">
-                    <option value="">Filter by Region</option>
-                    <option value="Africa">África</option>
+                <select name="region" id="" className="region-select-list" defaultValue="Filter by Region">
+                    <option disabled hidden>Filter by Region</option>
+                    <option value="" style={{fontSize:"5px"}} disabled>&nbsp;</option>
+                    <option className="option-list"value="Africa">África</option>
+                    <option value="" style={{fontSize:"5px"}} disabled>&nbsp;</option>
                     <option value="America">América</option>
+                    <option value="" style={{fontSize:"5px"}} disabled>&nbsp;</option>
                     <option value="Asia">Ásia</option>
+                    <option value="" style={{fontSize:"5px"}} disabled>&nbsp;</option>
                     <option value="Europa">Europa</option>
+                    <option value="" style={{fontSize:"5px"}} disabled>&nbsp;</option>
                     <option value="Oceania">Oceania</option>
+                    <option value="" style={{fontSize:"5px"}} disabled>&nbsp;</option>
                 </select>
            </div>
      )
