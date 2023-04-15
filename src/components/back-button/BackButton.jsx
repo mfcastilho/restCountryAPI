a5e7e57e2493a10f034759aceffc25ee3582be8a
mfcastilho <fredericocastilho@hotmail.com>
2023-04-14 23:30:02 -0300

    feat: implementing BackButton method

--- src/components/back-button/BackButton.jsx
+++ src/components/back-button/BackButton.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./BackButton.css";
+import { useNavigate } from "react-router-dom";
+
 
 function BackButton(){
+     const navigate = useNavigate();
+
+     function BackButton(){
+          navigate("/");
+     }
 
      return(
           <div className="back-button-container">
-               <button className="back-button">
+               <button onClick={BackButton} className="back-button">
                     <img src="../public/left-arrow.png" alt="" />
                     Back
                </button>
